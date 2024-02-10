a6b647254d2339f8c845c36677154f3575497809
Anna Ransbotham-Cole <annaransbotham-cole@annas-mbp.lan>
2024-02-10 16:45:59 -0600

    black formatting

--- create_tables.py
+++ create_tables.py
@@ -3,27 +3,33 @@ import psycopg2
 from redshift_utils import execute_sql_queries
 from sql_queries import create_table_queries, drop_table_queries
 
+
 def main():
-    #Access Config File
+    # Access Config File
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     # Establish Connection to Database
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config['CLUSTER'].values()
+        )
+    )
+
     # Create cursor object
     cur = conn.cursor()
 
     # Drop Tables
     print(f'Dropping Tables: { drop_table_queries }')
     execute_sql_queries(cur, conn, drop_table_queries)
-    
+
     # Create Tables
     print(f'Creating Tables: { create_table_queries }')
     execute_sql_queries(cur, conn, create_table_queries)
 
     # Close Connection
     conn.close()
+
 
 if __name__ == "__main__":
     main()
--- etl.py
+++ etl.py
@@ -3,20 +3,25 @@ import psycopg2
 from redshift_utils import execute_sql_queries
 from sql_queries import copy_table_queries, insert_table_queries
 
+
 def main():
     # Read from Config
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
     # Set Database Connection
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect(
+        "host={} dbname={} user={} password={} port={}".format(
+            *config['CLUSTER'].values()
+        )
+    )
     cur = conn.cursor()
-    
+
     # Load data into Staging Tables
     print(f'Loading data into Staging Tables: { copy_table_queries }')
     execute_sql_queries(cur, conn, copy_table_queries)
-    
-    # Load Data into Fact and Dimension Tables 
+
+    # Load Data into Fact and Dimension Tables
     print(f'Loading data into Fact and Dim Tables: { insert_table_queries }')
     execute_sql_queries(cur, conn, insert_table_queries)
 
